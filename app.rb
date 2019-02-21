61e8dfb550972ac85a279cb11478ff4add783f9e
jonmackeyparty <jon@jonmackey.party>
2019-02-21 11:32:09 -0500

    load w shotgun, add text to app.rb

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! Hell yeah boi I'm up in the browser!!!  Now I'm holding a loaded shotgun!!!"
   end
 
 end
