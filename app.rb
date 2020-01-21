0ac9df46c921150ec2e2ba1689dd4f65e00577cb
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-01-21 14:43:13 +0000

    shotgun

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Started my server using Shotgun!"
   end
 
 end
+
+#When you start an application with shotgun, all of your application code will be reloaded upon every request. 
