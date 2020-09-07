f2b3587e3cd90b40e886188f550a75630c023b34
sparsell <hello@sparsell.com>
2020-09-07 06:21:19 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I am going to be a great developer. Blockchain, here I come. "
   end
 
 end
