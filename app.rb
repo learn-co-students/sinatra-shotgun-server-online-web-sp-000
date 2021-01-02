a043ad467672ffd97578bd14021c40e9c924426e
Alan Tang <giabao_tang@yahoo.com>
2021-01-01 17:36:47 -0800

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I BUILT THIS. Please fuck me 5 times tonight."
   end
 
 end
